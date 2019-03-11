94000a79930f65992664d87cb23cf75b337d57d7
Duy Luong <duyluonglc@gmail.com>
2019-03-11 12:21:44 +0700

    check startup

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -2,14 +2,24 @@ import { put, select } from 'redux-saga/effects'
 import { is, call } from 'ramda'
 import { Actions, ActionConst } from 'react-native-router-flux'
 import { delay } from 'redux-saga'
+import { Alert } from 'react-native'
 
 // process STARTUP actions
 export function * startup (action) {
   // yield delay(1000)
   const auth = yield select(state => state.auth)
-  if (auth.refresh_token) {
+  if (auth.refresh_token && auth.user) {
     // call refresh token
-    yield call(Actions.root, { type: ActionConst.RESET })
+    if (auth.user.is_blocked) {
+      yield call(Actions.login, { type: ActionConst.RESET })
+      Alert.alert('Yoto', 'You are blocked')
+    } else if (!auth.user.instagram) {
+      yield call(Actions.register, { type: ActionConst.RESET })
+    } else if (!auth.user.profile_video_url && auth.user.profile_photo_url) {
+      yield call(Actions.upload, { type: ActionConst.RESET })
+    } else {
+      yield call(Actions.root, { type: ActionConst.RESET })
+    }
   } else {
     yield call(Actions.login, { type: ActionConst.RESET })
   }
